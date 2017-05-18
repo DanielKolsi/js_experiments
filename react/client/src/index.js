bedfb4b61d61ff266c691fe4d7f1412f46c9f953
Daniel Agape Kolsi <kolsi.daniel@gmail.com>
2017-05-17 23:30:43 -0500

    react template update

--- react/client/src/index.js
+++ react/client/src/index.js
@@ -9,16 +9,16 @@ ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
-var ExampleApplication = React.createClass({
-  render: function() {
+class ExampleApplication extends React.Component {
+  render() {
     var elapsed = Math.round(this.props.elapsed  / 100);
     var seconds = elapsed / 10 + (elapsed % 10 ? '' : '.0' );
     var message =
-      'React has been successfully running for ' + seconds + ' seconds.';
+      'React has been --- successfully running for ' + seconds + ' seconds.';
 
     return React.DOM.p(null, message);
   }
-});
+}
 
 // Call React.createFactory instead of directly call ExampleApplication({...}) in React.render
 var ExampleApplicationFactory = React.createFactory(ExampleApplication);
