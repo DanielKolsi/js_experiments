c68e2b2a28095dd2a9124d2f0e3979ec4d1e754f
Daniel Agape Kolsi <kolsi.daniel@gmail.com>
2017-05-23 17:59:50 -0500

    Chess-solution init

---
+++ react/chess-solution/README.md
@@ -0,0 +1,11 @@
+Chess solution engine for React
+
+The purpose of this engine is to determine who wins (or stalemate) in the chess in case of
+optimal chess moves.
+
+The solution is not based on brute force calculation but instaed of heuristics
+and game tree minimization with game situation related point functions.
+
+This React app is originally ported from a Java application
+
+Author: Daniel Kolsi
---
+++ react/chess-solution/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const PATHS = {
+  src: path.join(__dirname, 'src'),
+  build: path.join(__dirname, 'public')
+};
+
+module.exports = {
+  entry: `${PATHS.src}/Root.js`,
+  output: {
+    path: PATHS.build,
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  plugins: [
+    
+  ],
+  devServer: {
+    contentBase: PATHS.build,
+    inline: true,
+    port: 3002
+  },
+  devtool: 'eval-source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2017', 'react']
+        }
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'sass'],
+        include: `${PATHS.src}/assets`
+      }
+    ]
+  }
+}
