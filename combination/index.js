68d16c8ae4868eb3e8aec8f36709b59f2e76354d
Daniel Agape Kolsi <kolsi.daniel@gmail.com>
2017-05-30 22:29:32 -0500

    maps + more data

--- combination/index.js
+++ combination/index.js
@@ -30,44 +30,7 @@ class IPForm extends React.Component {
     );
   }
 }
-//var ips = '12'; // '130.233.220.240';
 
-/*var CommentBox = React.createClass({
-
-  getInitialState: function() {
-    console.log('initial state='+this.state.data);
-    return {data: []};
-  },
-
-  handleChange: function() {
-    console.log('just testing22');
-    return {data: []};
-  },
-
-  handleSubmit: function(ip_data) {
-    //ip = '66';
-    alert('submit_state='+this.state.data);
-    console.log('submit data2='+this.state.data);
-    //ips = "133";
-    return ip;
-    //return {data: []};
-  },
-
-  render: function() {
-    return (
-
-      <form onSubmit={this.handleSubmit}>
-         <label>
-           IP: hahah
-           <input type="text" value={this.state.value} onChange={this.handleChange} />
-         </label>
-         <input type="submit" value="Submit" />
-        </form>
-
-    );
-  }
-});
-*/
 ReactDOM.render(
   <IPForm />,
   document.getElementById('content')
