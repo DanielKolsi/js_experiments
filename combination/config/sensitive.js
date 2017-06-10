d9cafa4689f519087b01e859d1be6666720f641a
Daniel Kolsi <kolsi.daniel@gmail.com>
2017-06-10 17:21:38 -0500

    Create sensitive.js

--- combination/config/sensitive.js
+++ combination/config/sensitive.js
@@ -1,4 +1,4 @@
 // to be (later) left out from GitHub (.gitignore)  :)
 
-// sensitive keys & passwords
+// sensitive keys & passwords (this is currently not too sensitive... ;)
 var API_KEY = 'AIzaSyBjmdHr9PZaOoSyLNHd4gJMb1aF2VSJbqU';
